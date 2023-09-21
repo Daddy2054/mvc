6b62f951b19769358a088e7bbc0bf48ae313c78a
Jean <acalto2@gmail.com>
2023-09-21 23:56:18 +0200

    testing api

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,17 +1,28 @@
 package controllers
 
 import (
-	"log"
+	"encoding/json"
+	"mvc/services"
 	"net/http"
 	"strconv"
 )
 
-func GetUser(resp http.ResponseWriter,req *http.Request){
-	userId,err := (strconv.ParseInt(req.URL.Query().Get("user_id"),10,64))
+func GetUser(resp http.ResponseWriter, req *http.Request) {
+	userId, err := (strconv.ParseInt(req.URL.Query().Get("user_id"), 10, 64))
 	if err != nil {
+		resp.WriteHeader(http.StatusBadRequest)
+		resp.Write([]byte("user_id must be a number"))
 		// just return the bad request to the client
 		return
 	}
-//	user, err : =  
-	log.Printf("about to process user_id %v", userId)
+	user, err := services.GetUser(userId)
+	if err != nil {
+		resp.WriteHeader(http.StatusNotFound)
+		resp.Write([]byte(err.Error()))
+		// handle the err and return
+		return
+	}
+	//return user to client
+	jsonValue, _ := json.Marshal(user)
+	resp.Write(jsonValue)
 }
