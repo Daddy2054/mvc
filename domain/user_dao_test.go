219710cf2aa059d13f95e18e3cabc5dcd0f9e4d6
Jean <acalto2@gmail.com>
2023-09-25 23:10:23 +0200

    s2

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -10,19 +10,10 @@ func TestGetUserNoUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
 	assert.Nil(t, user, "we were not expecting a user id 0")
-	assert.NotNil(t,err)
-	assert.EqualValues(t,http.StatusNotFound,err.StatusCode)
-	
-	if user != nil {
-		t.Error("we were not expecting a user id 0")
-	}
-
-	if err == nil {
-		t.Error("we were expecting a user id 0")
-	}
-
-	if err.StatusCode != http.StatusNotFound {
-		t.Error("we were expecting 404 when a user not found")
-	}
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 does not exists", err.Message)
 
 }
+
