4c827fad2b6488f02d2b115fe2e1107dc01df86a
Jean <acalto2@gmail.com>
2023-09-29 22:32:58 +0200

    10. How to structure Go artifacts and mock p2

--- domain/user_dao.go
+++ domain/user_dao.go
@@ -11,9 +11,16 @@ var (
 	users = map[int64]*User{
 		123: {Id: 123, FirstName: "Fede", LastName: "Leon", Email: "a@a.com"},
 	}
-	UserDao userDao
+	UserDao userDaoInterface
 )
 
+func init() {
+	UserDao = &userDao{}
+} 
+type userDaoInterface interface{
+	GetUser(int64) (*User, *utils.ApplicationError)
+
+}
 type userDao struct{}
 
 func (u *userDao) GetUser(userId int64) (*User, *utils.ApplicationError) {
